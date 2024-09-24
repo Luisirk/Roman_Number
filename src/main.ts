a5d7480aab22418326dd70d5258b2c0330b808d0
Luis <luisirk.montoto@gmail.com>
2024-09-24 12:31:24 +0200

    Green: Refactor

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,13 @@
 export function add (expression: string) {
-  let number = Number(expression);
-  if(number === 1){
-    return 'I'
-  }
-  if(number === 5){
-    return 'V'
-  }
-  if(number === 10){
-    return 'X'
-  }
-  if(number === 50){
-    return 'L'
-  }
-  if(number === 100){
-    return 'C'
-  }
-  if(number === 500){
-    return 'D'
-  }
-  if(number === 1000){
-    return 'M'
-  }
-
-
-
-
+  const romanMap: { [key: number]: string } = {
+    1: 'I',
+    5: 'V',
+    10: 'X',
+    50: 'L',
+    100: 'C',
+    500: 'D',
+    1000: 'M'
+  };
+  const number = Number(expression);
+  return romanMap[number] || '';
 }
